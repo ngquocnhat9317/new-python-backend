c3050931936d7c1bce573c6657c60a903aaf67b0
Nguyễn Đoàn Quốc Nhật <48057924+ngquocnhat9317@users.noreply.github.com>
2023-12-19 16:41:49 +0000

    Push something new

--- app/main.py
+++ app/main.py
@@ -1,10 +1,28 @@
 from aiohttp import web
+import asyncio
 
-routes = web.RouteTableDef()
-
-@routes.get('/')
 async def hello(request):
     return web.Response(text="Hello, world")
 
-app = web.Application()
-web.run_app(app)
+async def bye(request):
+    return web.Response(text="Bye bye, world")
+
+def create_runner():
+    app = web.Application()
+    app.add_routes([
+        web.get('/', hello),
+        web.get('/bye', bye),
+    ])
+    return web.AppRunner(app)
+
+async def start_server(host="0.0.0.0", port=8080):
+    runner = create_runner()
+    await runner.setup()
+
+    site = web.TCPSite(runner, host, port)
+    await site.start()
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(start_server())
+    loop.run_forever()
